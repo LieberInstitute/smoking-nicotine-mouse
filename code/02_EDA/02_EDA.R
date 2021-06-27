6d90180ea2dfd6ec123c5c3c2dfe62cabfe46d76
bpardo99 <bree.pardo99@gmail.com>
2021-06-26 21:59:30 -0500

    Add code of gene expression variability and covariates

--- code/02_EDA/02_EDA.R
+++ code/02_EDA/02_EDA.R
@@ -8,162 +8,249 @@ library("recount")
 library("jaffelab")
 library("stats")
 library("ggfortify")
+library("scater")
+library("limma")
+library("edgeR")
 
-#load data
+# load data
 load(here("processed-data","build_objects","rse_gene.Rdata"), verbose = TRUE)
 load(here("processed-data","build_objects","rse_exon.Rdata"), verbose = TRUE)
 load(here("processed-data","build_objects","rse_jx.Rdata"), verbose = TRUE)
 load(here("processed-data","build_objects","rse_tx.Rdata"), verbose = TRUE)
 
-#Obtain PC
-geneExprs <- log2(recount::getRPKM(rse_gene, "Length") + 1) #check why add 1
+# Obtain PCa
+geneExprs <- log2(recount::getRPKM(rse_gene, "Length") + 1)
 set.seed(20201006)
 pca <- stats::prcomp(t(geneExprs))
 pca_vars <- jaffelab::getPcaVars(pca)
-pca_var_expl <- paste0(
+pca_vars_lab <- paste0(
   "PC", seq(along = pca_vars), ": ",
   pca_vars, "% Var Expl"
 )
 
-#Create data frame
+# Create data frame
 coldata <- as.data.frame(colData(rse_gene))
 pca_data <- cbind(coldata, pca$x)
 
-#Plot PCs
 
-#Age
-#PC1 vs PC2
+# Explore gene expression variability: PC vs PC
+## Age
+### PC1 vs PC2
 pdf(here("plots", "02_EDA", "pc-Age-plot.pdf"))
 ggplot(data =pca_data, aes(x=PC1,y=PC2,color= Age.x)) + 
   geom_point() + 
   ggtitle("PCA of the smoking mouse data, colored by age") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[2])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[2])
 
-#PC1 vs PC3
+### PC1 vs PC3
 ggplot(data =pca_data, aes(x=PC1,y=PC3,color= Age.x)) + 
   geom_point() + 
   ggtitle("PCA of the smoking mouse data, colored by age") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[3])
 
-#PC2 vs PC3
+### PC2 vs PC3
 ggplot(data =pca_data, aes(x=PC2,y=PC3,color= Age.x)) + 
   geom_point() +
   ggtitle("PCA of the smoking mouse data, colored by age") +
-  labs(y= pca_vars_lab[2], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[2], y = pca_vars_lab[3])
 dev.off()
 
 
-#Exposition
-#PC1 vs PC2
+## Exposition
+### PC1 vs PC2
 pdf(here("plots", "02_EDA", "pc-exposition-plot.pdf"))
 ggplot(data =pca_data, aes(x=PC1,y=PC2,color=Expt.x)) + 
   geom_point(aes(shape=Age.x))  +
   ggtitle("PCA of the smoking mouse data, colored by exposition") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[2])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[2])
 
-#PC1 vs PC3
+### PC1 vs PC3
 ggplot(data =pca_data, aes(x=PC1,y=PC3,color=Expt.x)) + 
   geom_point(aes(shape=Age.x))  +
   ggtitle("PCA of the smoking mouse data, colored by exposition") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[3])
 
-#PC2 vs PC3
+### PC2 vs PC3
 ggplot(data =pca_data, aes(x=PC2,y=PC3,color=Expt.x)) + 
   geom_point(aes(shape=Age.x))  +
   ggtitle("PCA of the smoking mouse data, colored by exposition") +
-  labs(y= pca_vars_lab[2], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[2], y = pca_vars_lab[3])
 dev.off()
 
 
-#Control vs cases
-#PC1 vs PC2
+##Control vs cases
+### PC1 vs PC2
 pdf(here("plots", "02_EDA", "pc-casecontrol-plot.pdf"))
 ggplot(data =pca_data, aes(x=PC1,y=PC2,color=Group.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by group") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[2])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[2])
 
-#PC1 vs PC3
+### PC1 vs PC3
 ggplot(data =pca_data, aes(x=PC1,y=PC3,color=Group.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by group") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[3])
 
-#PC2 vs PC3
+### PC2 vs PC3
 ggplot(data =pca_data, aes(x=PC2,y=PC3,color=Group.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by group") +
-  labs(y= pca_vars_lab[2], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[2], y = pca_vars_lab[3])
 dev.off()
 
 
-#Location
-#PC1 vs PC2
+## Location
+### PC1 vs PC2
 pdf(here("plots", "02_EDA", "pc-location-plot.pdf"))
 ggplot(data =pca_data, aes(x=PC1,y=PC2,color=location.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by location") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[2])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[2])
 
-#PC1 vs PC3
+### PC1 vs PC3
 ggplot(data =pca_data, aes(x=PC1,y=PC3,color=location.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by location") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[3])
 
-#PC2 vs PC3
+### PC2 vs PC3
 ggplot(data =pca_data, aes(x=PC2,y=PC3,color=location.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by location") +
-  labs(y= pca_vars_lab[2], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[2], y = pca_vars_lab[3])
 dev.off()
 
 
-#Plate
-#PC1 vs PC2
+## Plate
+### PC1 vs PC2
 pdf(here("plots", "02_EDA", "pc-plate-plot.pdf"))
 ggplot(data =pca_data, aes(x=PC1,y=PC2,color=plate.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by plate") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[2])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[2])
 
-#PC1 vs PC3
+### PC1 vs PC3
 ggplot(data =pca_data, aes(x=PC1,y=PC3,color=plate.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by plate") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[3])
 
-#PC2 vs PC3
+### PC2 vs PC3
 ggplot(data =pca_data, aes(x=PC2,y=PC3,color=plate.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by plate") +
-  labs(y= pca_vars_lab[2], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[2], y = pca_vars_lab[3])
 dev.off()
 
 
-#Tissue
-#PC1 vs PC2
+## Tissue
+### PC1 vs PC2
 pdf(here("plots", "02_EDA", "pc-tissue-plot.pdf"))
 ggplot(data =pca_data, aes(x=PC1,y=PC2,color=Tissue.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by tissue") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[2])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[2])
 
-#PC1 vs PC3
+### PC1 vs PC3
 ggplot(data =pca_data, aes(x=PC1,y=PC3,color=Tissue.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by tissue") +
-  labs(y= pca_vars_lab[1], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[1], y = pca_vars_lab[3])
 
-#PC2 vs PC3
+### PC2 vs PC3
 ggplot(data =pca_data, aes(x=PC2,y=PC3,color=Tissue.x)) + 
   geom_point(aes(shape=Age.x)) +
   ggtitle("PCA of the smoking mouse data, colored by tissue") +
-  labs(y= pca_vars_lab[2], x = pca_vars_lab[3])
+  labs(x= pca_vars_lab[2], y = pca_vars_lab[3])
 dev.off()
 
 
-#Boxplots
+## PCs vs SPEAQeasy output summary metrics
+pdf(here("plots", "02_EDA", "pc-metricsSPEAQeasy-plot.pdf"))
+### PC1
+ggplot(data =pca_data, aes(x=PC1,y=mitoMapped,color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[1])
+
+ggplot(data =pca_data, aes(x=PC1, y=mitoRate,color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[1])
+
+ggplot(data =pca_data, aes(x=PC1, y=totalAssignedGene,color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[1])
+
+ggplot(data =pca_data, aes(x=PC1, y=rRNA_rate, color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[1])
+
+ggplot(data =pca_data, aes(x=PC1, y=ERCCsumLogErr, color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[1])
+
+### PC2
+ggplot(data =pca_data, aes(x=PC2,y=mitoMapped,color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[2])
+
+ggplot(data =pca_data, aes(x=PC2, y=mitoRate,color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[2])
+
+ggplot(data =pca_data, aes(x=PC2, y=totalAssignedGene,color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[2])
+
+ggplot(data =pca_data, aes(x=PC2, y=rRNA_rate, color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[2])
+
+ggplot(data =pca_data, aes(x=PC2, y=ERCCsumLogErr, color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[2])
+
+
+### PC3
+ggplot(data =pca_data, aes(x=PC3,y=mitoMapped,color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[3])
+
+ggplot(data =pca_data, aes(x=PC3, y=mitoRate,color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[3])
+
+ggplot(data =pca_data, aes(x=PC3, y=totalAssignedGene,color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[3])
+
+ggplot(data =pca_data, aes(x=PC3, y=rRNA_rate, color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[3])
+
+ggplot(data =pca_data, aes(x=PC3, y=ERCCsumLogErr, color=Tissue.x)) + 
+  geom_point() +
+  ggtitle("PCA of the smoking mouse data, colored by tissue") +
+  labs(x= pca_vars_lab[3])
+dev.off()
+
+
+
+# Explore gene expression variability: PCs boxplots
 
 pc_list = pca_data[, which(colnames(pca_data)=="PC1" | colnames(pca_data)=="PC2"
                            | colnames(pca_data)=="PC3" | colnames(pca_data)=="PC4"
@@ -190,12 +277,81 @@ n=1
       geom_boxplot(colour = "grey", fill = "light grey") +
       geom_point(aes(color=group), position = "jitter") +
       ggtitle("Boxplot, PCA of the smoking mouse data") +
-      labs(y = pca_var_expl[j])
+      labs(x=colnames(var_list[i]) ,y = pca_vars_lab[j])
     n=n+1
    }
-   # print("done")
  }
 pdf(here("plots", "02_EDA", "boxplots.pdf"))
 lapply(plot_list, print)
 dev.off()
 
+
+
+# Explore gene expression variability: model for each gene
+## is this also performed for exons, jx and tx?
+
+## Diagnostic plots for quality control
+mito= subsets=list(Mito=grep("Mt_", rowData(rse_gene)$gene_type))
+rse_gene <- addPerCellQC(rse_gene, subsets=mito)
+
+### detected genes VS total count. 
+pdf(here("plots", "02_EDA", "diagnostic_qual_plots"))
+ggplot(data =coldata, aes(x=sum, y=detected, color= Tissue.x))+ 
+  geom_point() 
+
+### Mitochondrial content VS total count. 
+ggplot(data =coldata, aes(x=sum, y=subsets_Mito_percent, color= Tissue.x)) + 
+  geom_point() 
+
+### Highest expressed genes
+
+### Explanatory Variables
+rse_gene <- logNormCounts(rse_gene)
+vars <- getVarianceExplained(rse_gene, 
+                             variables=c("Tissue.x", "Age.x", "Sex.x", "Age.x", 
+                                         "Expt.x", "Group.x", "plate.x", "location.x"))
+plotExplanatoryVariables(vars)
+dev.off()
+
+
+
+## Applying variancePartition 
+
+### identify genes that pass expression cutoff
+geneCounts <- assays(rse_gene)$counts
+isexpr <- rowSums(cpm(geneCounts)>1) >= 0.5 * ncol(geneCounts)
+### create data structure with only expressed genes
+gExpr <- DGEList(counts=geneCounts[isexpr,])
+### Perform TMM normalization
+gExpr <- calcNormFactors(gExpr)
+### Specify variables to be included in the voom() estimates of uncertainty.
+design <- model.matrix(~ Tissue.x + Age.x, data=coldata)
+### Estimate precision weights for each gene and sample
+vobjGenes <- voom(gExpr, design)
+#form <- ~ (1|Individual) + (1|Tissue) + (1|Batch) + Age
+### Variance partition
+varPart <- fitExtractVarPartModel(vobjGenes, form, coldata)
+
+
+
+# Explore covariates
+pdf(here("plots", "02_EDA", "explore_metrics.pdf"))
+# for(var in c('totalAssignedGene', 'mitoRate', 'overallMapRate', #RIN does not exist
+#              'ERCCsumLogErr', 'rRNA_rate')) {
+#   boxplot(mean(colData(rse_gene)[, var]) ~ colData(rse_gene)$Tissue.x, 
+#           ylab = paste('mean', var), xlab = 'Tissue', col = 'light blue', 
+#           #main = paste('p-value:', signif(t.test(mean(colData(rse_gene)[, var]) ~ colData(rse_gene)$Tissue.x)$p.value, 3))
+#           )
+# }
+
+ggplot(data =coldata, aes(x=mean(colData(rse_gene)$mitoRate), 
+                          y=mean(colData(rse_gene)$rRNA_rate), color= Tissue.x)) + geom_point() 
+ggplot(data =coldata, aes(x=mean(colData(rse_gene)$mitoRate), y=mean(colData(rse_gene)$ERCCsumLogErr), color= Tissue.x)) + 
+  geom_point() 
+ggplot(data =coldata, aes(x=mean(colData(rse_gene)$rRNA_rate), y=mean(colData(rse_gene)$ERCCsumLogErr), color= Tissue.x)) + 
+  geom_point() 
+
+# boxplot(mean(colData(rse_gene)$mitoRate) ~ factor(mean(colData(rse_gene)$ERCCsumLogErr) > -200) * colData(rse_gene)$Tissue.x, col = 'light blue', ylab = 'mean mitoRate', xlab = 'mean ERCCsumLogErr > -200 by region')
+# boxplot(mean(colData(rse_gene)$rRNA_rate) ~ factor(mean(colData(rse_gene)$ERCCsumLogErr) > -200) * colData(rse_gene)$Tissue.x, col = 'light blue', ylab = 'mean rRNA_rate', xlab = 'mean ERCCsumLogErr > -200 by region')
+
+dev.off()
