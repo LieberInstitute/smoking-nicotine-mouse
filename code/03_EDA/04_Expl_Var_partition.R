288e0cf5e0b0b71b1ad2fa48e313f79f49a26404
Daianna Gonzalez <72838938+daianna21@users.noreply.github.com>
2022-07-24 14:17:24 -0500

    Add variables' colors

--- code/03_EDA/04_Expl_Var_partition.R
+++ code/03_EDA/04_Expl_Var_partition.R
@@ -23,12 +23,25 @@ expl_var<- function(type, tissue, age){
   }
   
   ## % of variance in gene expression explained by each variable
-  exp_vars<-getVarianceExplained(RSE, variables=c("Sex", "Pregnancy", 
-                    "Expt", "Group", "plate", "flowcell", "mitoRate", "rRNA_rate", 
-                    "overallMapRate", "totalAssignedGene", "ERCCsumLogErr"), 
-                    exprs_values = "logcounts")
-  ## Plot explanatory variables ordered by % of variance explained
-  plotExplanatoryVariables(exp_vars, theme_size = 10, nvars_to_plot = 12)
+  variables=c("Sex", "Pregnancy","Expt", "Group", "plate", "flowcell", 
+                                 "mitoRate", "rRNA_rate","overallMapRate", "totalAssignedGene", "ERCCsumLogErr")
+  exp_vars<-getVarianceExplained(RSE, variables=variables, exprs_values = "logcounts")
+  ## Plot explanatory variables 
+  ## Variables' colors
+  colors=c("Sex"="tomato3", "Pregnancy"="lightpink", "Expt"="slateblue2","Group"="seagreen3","plate"="tan1",
+           "flowcell"="mediumorchid1", "mitoRate"="royalblue1","rRNA_rate"="yellow3","overallMapRate"="wheat4", 
+           "totalAssignedGene"="violetred1", "ERCCsumLogErr"="ivory4")
+  values<-c()
+  for (variable in variables){
+    levels<-eval(parse_expr(paste("RSE$",variable, sep="")))
+    ## Drop variables with fewer than 2 unique levels
+    if (length(table(levels))>1){
+      values<-append(values, variable)
+    }
+  }
+  ## Plot density graph for each variable
+  p<-plotExplanatoryVariables(exp_vars, theme_size = 10, nvars_to_plot = 12)
+  p + scale_colour_manual(values = colors[c(values)]) + labs(color="Variables")
   ## Save plot
   if (is.null(age)){
     fileName=paste("plots/03_EDA/04_Expl_Var_partition/Expl_vars_density_",type,"_",tissue,".pdf", sep="")
@@ -46,10 +59,3 @@ expl_var<- function(type, tissue, age){
 expl_var("gene", "blood", NULL)
 expl_var("gene", "brain", "adults")
 expl_var("gene", "brain", "pups")
-
-
-
-
-
-
-
