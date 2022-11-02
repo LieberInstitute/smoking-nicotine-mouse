3454d6a6de342ab317363a5800d5e0226b2456cd
daianna21 <daianna2121@outlook.es>
2022-11-01 21:51:13 -0600

    Code for DEA

--- code/04_DEA/Exon_analysis/01_Modelling.R
+++ code/04_DEA/Exon_analysis/01_Modelling.R
@@ -1,5 +1,7 @@
 
 library(here)
+library(SummarizedExperiment)
+library(edgeR)
 
 
 ## 1. Differential Expression Analysis at the exon level
@@ -26,7 +28,7 @@ DEA_expt_vs_ctl<- function(RSE, formula, name, coef){
   
   ## Previous lib sizes of each sample
   match_samples <- match(RSE$SAMPLE_ID, samples_factors$SAMPLE_ID)
-  stopifnot(all(!is.na(m)))
+  stopifnot(all(!is.na(match_samples)))
   factors<-samples_factors[match_samples, ]
   
   pdf(file = paste("plots/04_DEA/01_Modelling/DEA_plots_", name, ".pdf", sep="" ))
@@ -40,30 +42,30 @@ DEA_expt_vs_ctl<- function(RSE, formula, name, coef){
   RSE_scaled$samples$norm.factors<-factors$norm.factors
   
   ## Transform counts to log2(CPM) 
-  ## Estimate mean-variance relationship for each gene
-  vGene = voom(RSE_scaled, design=model, plot=TRUE)
+  ## Estimate mean-variance relationship for each exon
+  vExon = voom(RSE_scaled, design=model, plot=TRUE)
   
-  ## Fit linear model for each gene 
-  fitGene = lmFit(vGene)
+  ## Fit linear model for each exon
+  fitGene = lmFit(vExon)
   
-  ## Empirical Bayesian calculation to obtain our significant genes:
   ## compute F and t-statistics, p-value and logFC for DE 
-  eBGene = eBayes(fitGene)
+  eBExon = eBayes(fitGene)
   
   ## Plot average log expression vs logFC
-  limma::plotMA(eBGene, coef = coef, xlab = "Mean of normalized counts", 
+  limma::plotMA(eBExon, coef = coef, xlab = "Mean of normalized counts", 
                 ylab="logFC")
-  ## Plot -log(p-value) vs logFC
-  volcanoplot(eBGene, coef = coef)
   
-  ## Select top-ranked genes for Group (expt vs ctrl)
-  top_genes = topTable(eBGene, coef=coef, p.value = 1, number=nrow(RSE), sort.by="none")
+  ## Plot -log(p-value) vs logFC
+  volcanoplot(eBExon, coef = coef)
+  
+  ## Select top-ranked exons for Group 
+  top_exons = topTable(eBExon, coef=coef, p.value = 1, number=nrow(RSE), sort.by="none")
   ## Histogram of adjusted p values
-  hist(top_genes$adj.P.Val, xlab="FDR", main="")
+  hist(top_exons$adj.P.Val, xlab="FDR", main="")
   
   dev.off()
   
-  return(list(top_genes, vGene, eBGene))
+  return(list(top_exons, vExon, eBExon))
   
 }  
 
