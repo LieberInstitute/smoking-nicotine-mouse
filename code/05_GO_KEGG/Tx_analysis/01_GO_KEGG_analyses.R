09c5aef59d29c60f67410cf3a4427c967a7c7c2a
daianna21 <daianna2121@outlook.es>
2023-01-11 21:47:50 -0600

    Start code for GO and KEGG analyses of DE txs' genes

---
+++ code/05_GO_KEGG/Tx_analysis/01_GO_KEGG_analyses.R
@@ -0,0 +1,14 @@
+
+# 1. Gene Ontology and KEGG analyses of DE txs' genes
+
+
+library(here)
+library(SummarizedExperiment)
+library(clusterProfiler)
+library(org.Mm.eg.db)
+library(jaffelab)
+library(ggplot2)
+library(cowplot)
+library(rlang)
+library(biomartr)
+library(sessioninfo)
