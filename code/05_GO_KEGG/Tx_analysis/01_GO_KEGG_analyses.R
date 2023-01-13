d78bbf4ef6ba233d8f88706d090012fa02509234
daianna21 <daianna2121@outlook.es>
2023-01-12 23:01:50 -0600

    Code to create boxplots of some interesting genes

--- code/05_GO_KEGG/Tx_analysis/01_GO_KEGG_analyses.R
+++ code/05_GO_KEGG/Tx_analysis/01_GO_KEGG_analyses.R
@@ -16,6 +16,8 @@ library(sessioninfo)
 
 load(here("processed-data/04_DEA/Tx_analysis/de_tx_nic.Rdata"))
 load(here("processed-data/04_DEA/Tx_analysis/de_tx_smo.Rdata"))
+load(here("processed-data/04_DEA/Exon_analysis/de_exons_nic.Rdata"))
+load(here("processed-data/04_DEA/Exon_analysis/de_exons_smo.Rdata"))
 load(here("processed-data/04_DEA/Tx_analysis/top_tx_nic.Rdata"))
 load(here("processed-data/04_DEA/Tx_analysis/top_tx_smo.Rdata"))
 load(here("processed-data/04_DEA/Gene_analysis/de_genes_pups_nicotine_fitted.Rdata"))
@@ -346,37 +348,39 @@ GO_KEGG_no_txs_genes("smoking")
 
 compare_DE <- function(expt){
   
-  top_exons<-eval(parse_expr(paste("top_exons_", substr(expt,1,3), sep="")))
-  top_genes<-eval(parse_expr(paste("top_genes_pups_", expt, "_fitted", sep="")))
-  top_tx<-eval(parse_expr(paste("top_tx", substr(expt,1,3), sep="")))
   de_genes<-eval(parse_expr(paste("de_genes_pups_", expt, "_fitted", sep="")))
   de_exons<-eval(parse_expr(paste("de_exons_", substr(expt,1,3), sep="")))
-  de_tx<-eval(parse_expr(paste("de_tx", substr(expt,1,3), sep="")))
-  
-  de_tx_Entrez <- extract_entrez(de_tx$ensemblID)$entrezgene_id
+  de_tx<-eval(parse_expr(paste("de_tx_", substr(expt,1,3), sep="")))
   
   ## Define groups of genes
   
   ## DEG with DE txs and exons
-  DE_all <- intersect(de_genes$EntrezID, de_exons$EntrezID, de_tx_Entrez)
+  DE_all <- intersect(intersect(de_genes$ensemblID, de_exons$ensemblID), de_tx$ensemblID)
+  DE_all <- extract_entrez(unique(DE_all))$entrezgene_id
   ## DEG with DE exons but no DE txs
-  DE_genes_exons <- intersect(de_genes$EntrezID, de_exons$EntrezID)
-  DE_genes_exons <- DE_genes_exons[which(! DE_genes_exons %in% de_tx_Entrez)]
+  DE_genes_exons <- intersect(de_genes$ensemblID, de_exons$ensemblID)
+  DE_genes_exons <- DE_genes_exons[which(! DE_genes_exons %in% de_tx$ensemblID)]
+  DE_genes_exons <- extract_entrez(unique(DE_genes_exons))$entrezgene_id
   ## DEG with DE txs but no DE exons
-  DE_genes_txs <- intersect(de_genes$EntrezID, de_tx_Entrez)
-  DE_genes_txs <- DE_genes_txs[which(! DE_genes_txs %in% de_exons$EntrezID)]
+  DE_genes_txs <- intersect(de_genes$ensemblID, de_tx$ensemblID)
+  DE_genes_txs <- DE_genes_txs[which(! DE_genes_txs %in% de_exons$ensemblID)]
+  DE_genes_txs <- extract_entrez(unique(DE_genes_txs))$entrezgene_id
   ## Non-DE genes with DE exons and txs
-  DE_exons_txs <- intersect(de_exons$EntrezID, de_tx_Entrez)
-  DE_exons_txs <- DE_exons_txs[which(! DE_exons_txs %in% de_genes$EntrezID)]
+  DE_exons_txs <- intersect(de_exons$ensemblID, de_tx$ensemblID)
+  DE_exons_txs <- DE_exons_txs[which(! DE_exons_txs %in% de_genes$ensemblID)]
+  DE_exons_txs <- extract_entrez(unique(DE_exons_txs))$entrezgene_id
   ## DEG without DE exons or txs
-  DE_genes <- union(de_genes[which(! de_genes$EntrezID %in% de_exons$EntrezID), "EntrezID"],
-                    de_genes[which(! de_genes$EntrezID %in% de_tx_Entrez)])
+  DE_genes <- intersect(de_genes[which(! de_genes$ensemblID %in% de_exons$ensemblID), "ensemblID"],
+                    de_genes[which(! de_genes$ensemblID %in% de_tx$ensemblID), "ensemblID"])
+  DE_genes <- extract_entrez(unique(DE_genes))$entrezgene_id
   ## DEE from non-DE genes without DE txs
-  DE_exons <- union(de_exons[which(! de_exons$EntrezID %in% de_genes$EntrezID), "EntrezID"],
-                    de_exons[which(! de_exons$EntrezID %in% de_tx_Entrez)])
+  DE_exons <- intersect(de_exons[which(! de_exons$ensemblID %in% de_genes$ensemblID), "ensemblID"],
+                    de_exons[which(! de_exons$ensemblID %in% de_tx$ensemblID), "ensemblID"])
+  DE_exons <- extract_entrez(unique(DE_exons))$entrezgene_id
   ## DEtxs from non-DE genes without DE exons
-  DE_txs <- union(de_tx[which(! de_tx_Entrez %in% de_genes$EntrezID), "EntrezID"],
-                  de_tx[which(! de_tx_Entrez %in% de_exons$EntrezID)])
+  DE_txs <- intersect(de_tx[which(! de_tx$ensemblID %in% de_genes$ensemblID), "ensemblID"],
+                  de_tx[which(! de_tx$ensemblID %in% de_exons$ensemblID), "ensemblID"])
+  DE_txs <- extract_entrez(unique(DE_txs))$entrezgene_id
   
   sigGeneList <- list("3 levels"= DE_all, "DEG & DEE"=DE_genes_exons, 
                       "DEG & DEtxs"= DE_genes_txs, "DEE & DEtxs"= DE_exons_txs, 
@@ -418,6 +422,306 @@ vGene_nic$E<-cleaningY(vGene_nic$E, model, P=2)
 rownames(vGene_smo$E)<-vGene_smo$genes$Symbol
 
 
+## Data frame for a single gene with its nicotine and smoking logcounts 
+get_df_DEG<- function(gene, vGene_nic, vGene_smo) {
+  
+  ## Logcounts for that gene 
+  logcounts_nic<-vGene_nic$E[which(rownames(vGene_nic)==gene),]
+  logcounts_smo<-vGene_smo$E[which(rownames(vGene_smo)==gene),]
+  df_nic<-data.frame("Gene_counts"=logcounts_nic, "Expt"=rep("Nicotine", length(logcounts_nic)),
+                     "Group"=vGene_nic$targets$Group, "SampleID"=vGene_nic$targets$SAMPLE_ID)
+  df_smo<-data.frame("Gene_counts"=logcounts_smo, "Expt"=rep("Smoking", length(logcounts_smo)),
+                     "Group"=vGene_smo$targets$Group, "SampleID"=vGene_smo$targets$SAMPLE_ID)
+  df<-rbind(df_nic, df_smo)
+  return(df)
+  
+}
+
+
+## Boxplot to compare the nicotine vs smoking lognorm counts for a single gene
+DEG_GO_boxplot <- function(DEgene){
+  
+  ## Extract necessary data
+  df<-get_df_DEG(gene = DEgene, vGene_nic, vGene_smo)
+  ## Extract Ensembl ID of the gene
+  ensemblID<-top_genes_pups_nicotine_fitted[which(top_genes_pups_nicotine_fitted$Symbol==DEgene), "ensemblID"]
+  
+  ## q-value for the gene in nicotine and smoking 
+  q_value_nic<-signif(top_genes_pups_nicotine_fitted[which(top_genes_pups_nicotine_fitted$Symbol==DEgene),
+                                                     "adj.P.Val"], digits = 3)
+  q_value_smo<-signif(top_genes_pups_smoking_fitted[which(top_genes_pups_smoking_fitted$Symbol==DEgene),
+                                                    "adj.P.Val"], digits = 3)
+  ## Log FC for the gene in nicotine and smoking 
+  FC_nic<-signif(2**(top_genes_pups_nicotine_fitted[which(top_genes_pups_nicotine_fitted$Symbol==DEgene),
+                                                    "logFC"]), digits = 3)
+  FC_smo<-signif(2**(top_genes_pups_smoking_fitted[which(top_genes_pups_smoking_fitted$Symbol==DEgene),
+                                                   "logFC"]), digits = 3)
+  
+  ## Boxplot for each DE gene
+  p <-ggplot(data=as.data.frame(df), aes(x=Group,y=Gene_counts)) + 
+    geom_boxplot(outlier.color = "#FFFFFFFF") +
+    geom_jitter(aes(color=Group), position=position_jitter(0.2)) +
+    theme_classic() +
+    labs(x = "Experiment", y = "logcounts - covariates",
+         title = paste(DEgene, ensemblID, sep=" - "),
+         subtitle=" ") +
+    theme(plot.margin=unit (c (1,1.5,1,1), 'cm'), legend.position = "none",
+          plot.title = element_text(hjust=0.5, size=10, face="bold"), 
+          plot.subtitle = element_text(size=17)) +
+    scale_color_manual(values = c("orangered", "Dodgerblue")) +
+    facet_wrap(~ Expt, scales = "free") +
+    scale_x_discrete(labels=c("Ctrl", "Expt"))
+  
+  p <-ggdraw(p) + 
+    draw_label(paste("FDR:", q_value_nic), x = 0.35, y = 0.87, size=9, color = "darkslategray") +
+    draw_label(paste("FC:", FC_nic), x = 0.35, y = 0.84, size=9, color = "darkslategray") +
+    draw_label(paste("FDR:", q_value_smo), x = 0.72, y = 0.87, size=9, color = "darkslategray") +
+    draw_label(paste("FC:", FC_smo), x = 0.71, y = 0.84, size=9, color = "darkslategray") 
+  
+  return(p)
+  
+}
+
+
+## Extract genes from each BP, CC, MF and KEGG descriptions
+GO_KEGG_genes<- function(golist, term, cluster, description){
+  
+  GOdata<-as.data.frame(eval(parse_expr(paste(golist, "$", term, sep=""))))
+  genes<-unique(GOdata[which(GOdata$Description==description & GOdata$Cluster==cluster), "geneID"])
+  genes<-strsplit(genes, "/")
+  genes<-unique(unlist(genes))
+  
+  if (term=="KEGG"){
+    ## KEGG ids (entrez ids) to gene symbols
+    symbols<-biomart(genes  = genes,
+                     mart       = "ENSEMBL_MART_ENSEMBL",
+                     dataset    = "mmusculus_gene_ensembl",
+                     attributes = c("external_gene_name"),
+                     filters    = "entrezgene_id")
+    genes<-symbols$external_gene_name
+  }
+  return(genes)
+  
+}
+
+
+## Extract top 6 genes from a list based on their max q-values 
+## in nicotine and smoking 
+extract_top_genes <- function(DEG_list){
+  
+  ## If the genes are only from smoking 
+  if (length(which(DEG_list %in% de_genes_pups_nicotine_fitted$Symbol))==0){
+    
+    ## q-values for the gene  
+    q_values_smo<-top_genes_pups_smoking_fitted[which(top_genes_pups_smoking_fitted$Symbol %in% DEG_list),
+                                                c("Symbol", "adj.P.Val")]
+    q_values_smo$adj.P.Val<-signif(q_values_smo$adj.P.Val, digits = 3)
+    ## Genes with the lowest q-values
+    genes<-q_values_smo[order(q_values_smo$adj.P.Val),1]
+    
+  }
+  
+  ## If the genes are only from nicotine
+  else if (length(which(DEG_list %in% de_genes_pups_smoking_fitted$Symbol))==0){
+    
+    ## q-values for the gene  
+    q_values_nic<-top_genes_pups_nicotine_fitted[which(top_genes_pups_nicotine_fitted$Symbol %in% DEG_list),
+                                                 c("Symbol", "adj.P.Val")]
+    q_values_nic$adj.P.Val<-signif(q_values_nic$adj.P.Val, digits = 3)
+    ## Genes with the lowest q-values
+    genes<-q_values_nic[order(q_values_nic$adj.P.Val),1]
+    
+  }
+  
+  else {
+    
+    nic_qvals<-vector()
+    smo_qvals<-vector()
+    max_qvals<-vector()
+    
+    for (DEgene in DEG_list){
+      ## q-values for the gene in nicotine and smoking 
+      q_value_nic<-signif(top_genes_pups_nicotine_fitted[which(top_genes_pups_nicotine_fitted$Symbol==DEgene),
+                                                         "adj.P.Val"], digits = 3)
+      q_value_smo<-signif(top_genes_pups_smoking_fitted[which(top_genes_pups_smoking_fitted$Symbol==DEgene),
+                                                        "adj.P.Val"], digits = 3)
+      nic_qvals<-append(nic_qvals, q_value_nic)
+      smo_qvals<-append(smo_qvals, q_value_smo)
+      ## Max q-value
+      max_qvals<-append(max_qvals, max(q_value_nic, q_value_smo))
+    }
+    
+    q_vals<-data.frame(Gene=DEG_list, Nicotine=nic_qvals, Smoking=smo_qvals, Max=max_qvals)
+    
+    ## Genes with the lowest max q-values
+    genes<-q_vals[order(q_vals$Max),1]
+  }
+  
+  
+  if (length(genes)<6){
+    return(genes)
+  }
+  else {
+    return(genes[1:6])
+  }
+}
+
+
+## Boxplots of the first 6 genes involved in a process/pathway
+GO_KEGG_boxplots<-function(DEG_list, description, cluster){
+  plots<-list()
+  i=1
+  for (DEgene in DEG_list){
+    plots[[i]]<-DEG_GO_boxplot(DEgene)
+    i=i+1
+  }
+  if (length(DEG_list)<6){
+    for (i in (length(plots)+1):6){
+      plots[[i]]<-NA
+    }
+  }
+  
+  
+  options(warn = - 1)   
+  plot_grid(plots[[1]], plots[[2]], plots[[3]], plots[[4]], plots[[5]], plots[[6]], ncol=3)
+  ggsave(here(paste("plots/05_GO_KEGG/Tx_analysis/Top", length(DEG_list), "_", description,"_boxplots_",cluster, 
+                    ".pdf", sep="")), width = 40, height = 25, units = "cm") 
+  
+}
 
 
 
+## Boxplots 
+
+## 1. Cellular components
+
+## Genes in glutamatergic synapses
+GO_genes<-GO_KEGG_genes("goList_global", "CC", "up", "glutamatergic synapse")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "glutamatergic_synapse", "up")
+
+GO_genes<-GO_KEGG_genes("goList_smo", "CC", "up", "glutamatergic synapse")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "glutamatergic_synapse", "smo_up")
+
+GO_genes<-GO_KEGG_genes("goList_intersections", "CC", "Only up smo", "glutamatergic synapse")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "glutamatergic_synapse", "Only_up_smo")
+
+GO_genes<-GO_KEGG_genes("goList_noDEG_smo", "CC", "non−DEG with DEtxs", "glutamatergic synapse")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "glutamatergic_synapse", "noDEG_smo")
+
+
+## Genes in SNARE complex
+GO_genes<-GO_KEGG_genes("goList_global", "CC", "up", "SNARE complex")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "SNARE_complex", "up")
+
+GO_genes<-GO_KEGG_genes("goList_smo", "CC", "up", "SNARE complex")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "SNARE_complex", "smo_up")
+
+GO_genes<-GO_KEGG_genes("goList_intersections", "CC", "Only up smo", "SNARE complex")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "SNARE_complex", "Only_up_smo")
+
+
+## Genes in transport vesicle
+GO_genes<-GO_KEGG_genes("goList_global", "CC", "up", "transport vesicle")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "transport_vesicle", "up")
+
+GO_genes<-GO_KEGG_genes("goList_smo", "CC", "up", "transport vesicle")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "transport_vesicle", "smo_up")
+
+GO_genes<-GO_KEGG_genes("goList_intersections", "CC", "Only up smo", "transport vesicle")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "transport_vesicle", "Only_up_smo")
+
+GO_genes<-GO_KEGG_genes("goList_noDEG_smo", "CC", "non−DEG with DEtxs", "transport vesicle")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "transport_vesicle", "noDEG_smo")
+
+
+## Genes in synaptic membrane
+GO_genes<-GO_KEGG_genes("goList_noDEG_smo", "CC", "non−DEG with DEtxs", "synaptic membrane")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "synaptic_membrane", "noDEG_smo")
+
+
+## Genes in presynapses
+GO_genes<-GO_KEGG_genes("goList_noDEG_smo", "CC", "non−DEG with DEtxs", "presynapse")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "presynapse", "noDEG_smo")
+
+
+## Genes in retromer complex (See PMID: 26199408)
+GO_genes<-GO_KEGG_genes("goList_noDEG_smo", "CC", "non−DEG with DEtxs", "retromer complex")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "retromer_complex", "noDEG_smo")
+
+
+
+## 2. Pathways
+
+## Genes involved in Pathways of neurodegeneration − multiple diseases
+GO_genes<-GO_KEGG_genes("goList_global", "KEGG", "up", "Pathways of neurodegeneration − multiple diseases")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "Neurodegeneration", "up")
+
+GO_genes<-GO_KEGG_genes("goList_smo", "KEGG", "up", "Pathways of neurodegeneration − multiple diseases")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "Neurodegeneration", "smo_up")
+
+
+## Genes involved in Parkinson disease
+GO_genes<-GO_KEGG_genes("goList_global", "KEGG", "up", "Parkinson disease")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "Parkinson_disease", "up")
+
+GO_genes<-GO_KEGG_genes("goList_smo", "KEGG", "up", "Parkinson disease")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "Parkinson_disease", "smo_up")
+ 
+GO_genes<-GO_KEGG_genes("goList_intersections", "KEGG", "Only up smo", "Parkinson disease")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "Parkinson_disease", "Only_up_smo")
+
+
+## Genes involved in Prion disease
+GO_genes<-GO_KEGG_genes("goList_global", "KEGG", "up", "Prion disease")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "Prion_disease", "up")
+
+GO_genes<-GO_KEGG_genes("goList_smo", "KEGG", "up", "Prion disease")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "Prion_disease", "smo_up")
+
+GO_genes<-GO_KEGG_genes("goList_intersections", "KEGG", "Only up smo", "Prion disease")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "Prion_disease", "Only_up_smo")
+
+
+## Genes involved in Huntington disease
+GO_genes<-GO_KEGG_genes("goList_intersections", "KEGG", "Only up smo", "Huntington disease")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "Huntington_disease", "Only_up_smo")
+
+
+## Genes involved in SNARE interactions in vesicular transport
+GO_genes<-GO_KEGG_genes("goList_intersections", "KEGG", "Smo up, nic down", "SNARE interactions in vesicular transport")
+top_DEG<-extract_top_genes(GO_genes)
+GO_KEGG_boxplots(top_DEG, "SNARE_interactions", "smoUp_nicDown")
+
+
+
+
+
+
+
+## Reproducibility information
+
+options(width = 120)
+session_info()
