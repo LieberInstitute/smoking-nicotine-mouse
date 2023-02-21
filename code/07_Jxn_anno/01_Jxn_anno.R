e31ee6568eb628b9e8a02cf030e0ab020ed54abb
daianna21 <daianna2121@outlook.es>
2023-02-21 12:54:46 -0600

    Add "isFusion" class information

--- code/07_Jxn_anno/01_Jxn_anno.R
+++ code/07_Jxn_anno/01_Jxn_anno.R
@@ -48,12 +48,12 @@ table(rowData(rse_jx)[which(rowData(rse_jx)$isFusion=="TRUE"), c("inGencodeStart
 
 
 ## Create table with the following info for each jxn class:
-
 ##        - number: total number of jxns from that class
 ##        - number_DEjxns_nic: number of nicotine DE jxns from that class
 ##        - percentage_DEjxns_nic: % of nicotine DE jxns that were from that class
 ##        - number_DEjxns_smo: number of smoking DE jxns from that class
 ##        - percentage_DEjxns_smo: % of smoking DE jxns that were from that class
+
 jxn_classes <- lapply(unique(rowData(rse_jx)$Class), function(x){cbind(Class=x, 
                                                         number_jxs=length(which(rowData(rse_jx)$Class==x)), 
                                                         number_DEjxns_nic=length(which(de_jxns_nic$Class==x)),
@@ -62,24 +62,25 @@ jxn_classes <- lapply(unique(rowData(rse_jx)$Class), function(x){cbind(Class=x,
                                                         percentage_DEjxns_smo=signif(length(which(de_jxns_smo$Class==x))/dim(de_jxns_smo)[1]*100, 3))})
 jxn_classes <- rbind(jxn_classes[[1]], jxn_classes[[2]], jxn_classes[[3]], jxn_classes[[4]])
 
-## Add "isFusion" class
-jxn_classes <- cbind("isFusion", 
-                     length(which(rowData(rse_jx)$isFusion=="TRUE")), 
-                     number_DEjxns_nic=length(which(de_jxns_nic$Class=="InGen")),
-                     percentage_DEjxns_nic=signif(length(which(de_jxns_nic$Class=="InGen"))/dim(de_jxns_nic)[1]*100, 3),
-                     number_DEjxns_smo=length(which(de_jxns_smo$Class=="InGen")),
-                     percentage_DEjxns_smo=signif(length(which(de_jxns_smo$Class=="InGen"))/dim(de_jxns_smo)[1]*100, 3))
+## Add "isFusion" class information 
+jxn_classes <- as.data.frame(rbind(jxn_classes, cbind("isFusion", 
+                                                       length(which(rowData(rse_jx)$isFusion=="TRUE")), 
+                                                       length(which(de_jxns_nic$isFusion=="TRUE")),
+                                                       signif(length(which(de_jxns_nic$isFusion=="TRUE"))/dim(de_jxns_nic)[1]*100, 3),
+                                                       length(which(de_jxns_smo$isFusion=="TRUE")),
+                                                       signif(length(which(de_jxns_smo$isFusion=="TRUE"))/dim(de_jxns_smo)[1]*100, 3))))
+
+jxn_classes$number_jxs <- as.numeric(jxn_classes$number_jxs)
+jxn_classes$number_DEjxns_nic <- as.numeric(jxn_classes$number_DEjxns_nic)
+jxn_classes$percentage_DEjxns_nic<- as.numeric(jxn_classes$percentage_DEjxns_nic)
+jxn_classes$number_DEjxns_smo <- as.numeric(jxn_classes$number_DEjxns_smo)
+jxn_classes$percentage_DEjxns_smo<- as.numeric(jxn_classes$percentage_DEjxns_smo)
+
+save(jxn_classes, file="processed-data/07_Jxn_anno/jxn_classes.Rdata")
 
 
 
-
-
-
-
-
-
-
-## Obtain novel DE introns/jxns
+## Obtain novel DE introns/jxns (with at least one unknown end or if the combination of ends is unknown)
 
 ## Nicotine
 novel_jxns_nic <- de_jxns_nic[which(! (de_jxns_nic$Class=="InGen")),]
