7e189915583250e9b37686945bb9e58dd8128263
daianna21 <daianna2121@outlook.es>
2023-02-21 23:44:56 -0600

    Create table with the information of DE jxns' genes

--- code/07_Jxn_anno/01_Jxn_anno.R
+++ code/07_Jxn_anno/01_Jxn_anno.R
@@ -48,6 +48,7 @@ table(rowData(rse_jx)[which(rowData(rse_jx)$isFusion=="TRUE"), c("inGencodeStart
 
 
 ## Create table with the following info for each jxn class:
+##        - Class: jxn class
 ##        - number: total number of jxns from that class
 ##        - number_DEjxns_nic: number of nicotine DE jxns from that class
 ##        - percentage_DEjxns_nic: % of nicotine DE jxns that were from that class
@@ -55,11 +56,11 @@ table(rowData(rse_jx)[which(rowData(rse_jx)$isFusion=="TRUE"), c("inGencodeStart
 ##        - percentage_DEjxns_smo: % of smoking DE jxns that were from that class
 
 jxn_classes <- lapply(unique(rowData(rse_jx)$Class), function(x){cbind(Class=x, 
-                                                        number_jxs=length(which(rowData(rse_jx)$Class==x)), 
-                                                        number_DEjxns_nic=length(which(de_jxns_nic$Class==x)),
-                                                        percentage_DEjxns_nic=signif(length(which(de_jxns_nic$Class==x))/dim(de_jxns_nic)[1]*100, 3),
-                                                        number_DEjxns_smo=length(which(de_jxns_smo$Class==x)),
-                                                        percentage_DEjxns_smo=signif(length(which(de_jxns_smo$Class==x))/dim(de_jxns_smo)[1]*100, 3))})
+                                                                       number_jxs=length(which(rowData(rse_jx)$Class==x)), 
+                                                                       number_DEjxns_nic=length(which(de_jxns_nic$Class==x)),
+                                                                       percentage_DEjxns_nic=signif(length(which(de_jxns_nic$Class==x))/dim(de_jxns_nic)[1]*100, 3),
+                                                                       number_DEjxns_smo=length(which(de_jxns_smo$Class==x)),
+                                                                       percentage_DEjxns_smo=signif(length(which(de_jxns_smo$Class==x))/dim(de_jxns_smo)[1]*100, 3))})
 jxn_classes <- rbind(jxn_classes[[1]], jxn_classes[[2]], jxn_classes[[3]], jxn_classes[[4]])
 
 ## Add "isFusion" class information 
@@ -79,6 +80,61 @@ jxn_classes$percentage_DEjxns_smo<- as.numeric(jxn_classes$percentage_DEjxns_smo
 save(jxn_classes, file="processed-data/07_Jxn_anno/jxn_classes.Rdata")
 
 
+## Create table with the information of DE jxns' genes:
+##        - Gene: ID of gene with DE jxns
+##        - number_DEjxns_nic: number of nicotine DE jxns the gene has
+##        - number_DEjxns_nic_Novel: number of nicotine DE novel jxns the gene has
+##        - number_DEjxns_nic_AltStartEnd: number of nicotine DE jxns with alternative start/end, the gene has
+##        - number_DEjxns_nic_InGen: number of nicotine DE known (in Gencode) jxns the gene has
+##        - number_DEjxns_nic_ExonSkip: number of nicotine DE jxns from non-successive exons, the gene has
+##        - number_DEjxns_smo: number of smoking DE jxns the gene has
+##        - number_DEjxns_smo_Novel: number of smoking DE novel jxns the gene has
+##        - number_DEjxns_smo_AltStartEnd: number of smoking DE jxns with alternative start/end, the gene has
+##        - number_DEjxns_smo_InGen: number of smoking DE known (in Gencode) jxns the gene has
+##        - number_DEjxns_smo_ExonSkip: number of smoking DE jxns from non-successive exons, the gene has
+
+## Genes with DE jxns
+DEjxns_genes <- union(unique(de_jxns_nic$newGeneID), unique(de_jxns_smo$newGeneID))
+DEjxns_genes <- DEjxns_genes[which(! is.na(DEjxns_genes))]
+## Info
+DEjxns_genes_info <- vector()
+for (gene in DEjxns_genes){
+          DEjxns_genes_info <- rbind(DEjxns_genes_info, 
+                                    c(Gene=gene,
+                                      number_DEjxns_nic=length(which(de_jxns_nic$newGeneID==gene)),
+                                      number_DEjxns_nic_Novel=length(which(de_jxns_nic$newGeneID==gene & de_jxns_nic$Class=="Novel")),
+                                      number_DEjxns_nic_AltStartEnd=length(which(de_jxns_nic$newGeneID==gene & de_jxns_nic$Class=="AltStartEnd")),
+                                      number_DEjxns_nic_InGen=length(which(de_jxns_nic$newGeneID==gene & de_jxns_nic$Class=="InGen")),
+                                      number_DEjxns_nic_ExonSkip=length(which(de_jxns_nic$newGeneID==gene & de_jxns_nic$Class=="ExonSkip")),
+                                      number_DEjxns_smo=length(which(de_jxns_smo$newGeneID==gene)),
+                                      number_DEjxns_smo_Novel=length(which(de_jxns_smo$newGeneID==gene & de_jxns_smo$Class=="Novel")),
+                                      number_DEjxns_smo_AltStartEnd=length(which(de_jxns_smo$newGeneID==gene & de_jxns_smo$Class=="AltStartEnd")),
+                                      number_DEjxns_smo_InGen=length(which(de_jxns_smo$newGeneID==gene & de_jxns_smo$Class=="InGen")),
+                                      number_DEjxns_smo_ExonSkip=length(which(de_jxns_smo$newGeneID==gene & de_jxns_smo$Class=="ExonSkip"))))
+}
+## Char to numeric
+DEjxns_genes_info <- cbind(Gene=DEjxns_genes_info[,1], as.data.frame(apply(DEjxns_genes_info[,2:11], 2, function(x){as.numeric(as.character(x))})))
+save(DEjxns_genes_info, file="processed-data/07_Jxn_anno/DEjxns_genes_info.Rdata")
+
+
+## Boxplot of the number of DE jxns per gene
+pdf(file = paste("plots/07_Jxn_anno/Number_DEjxns_per_gene.pdf", sep="" ))
+par(mfrow=c(2,2))
+
+ggplot(data=DEjxns_genes_info, aes(x=0,y=number_DEjxns_nic)) + 
+      geom_boxplot() +
+      theme_classic() +
+      labs(x = 0, y = "Number of nicotine DE jxns per gene") 
+
+ggplot(data=DEjxns_genes_info, aes(x=0,y=number_DEjxns_smo)) + 
+      geom_boxplot() +
+      theme_classic() +
+      labs(x = 0, y = "Number of smoking DE jxns per gene") 
+
+dev.off()
+
+
+
 
 ## Obtain novel DE introns/jxns (with at least one unknown end or if the combination of ends is unknown)
 
@@ -86,6 +142,8 @@ save(jxn_classes, file="processed-data/07_Jxn_anno/jxn_classes.Rdata")
 novel_jxns_nic <- de_jxns_nic[which(! (de_jxns_nic$Class=="InGen")),]
 ## Smoking
 novel_jxns_smo <- de_jxns_smo[which(! (de_jxns_smo$Class=="InGen")),]
+
+## Find nearest genes of novel jxns
 
 
 
