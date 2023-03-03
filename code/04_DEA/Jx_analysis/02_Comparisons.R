ba7c914643e4d24442b6ce7148f49e2dc6e82a01
daianna21 <daianna2121@outlook.es>
2023-03-02 19:24:57 -0600

    Venn diagrams of DEG vs DE txs' genes vs DE exons' genes vs DE jxns' genes (from all classes or particular ones)

--- code/04_DEA/Jx_analysis/02_Comparisons.R
+++ code/04_DEA/Jx_analysis/02_Comparisons.R
@@ -3,6 +3,7 @@
 
 load(here("processed-data/04_DEA/Jx_analysis/de_jxns_nic.Rdata"))
 load(here("processed-data/04_DEA/Jx_analysis/de_jxns_smo.Rdata"))
+load(here("processed-data/07_Jxn_anno/novel_jxns_foundGenes.Rdata"))
 
 load(here("processed-data/04_DEA/Tx_analysis/de_tx_nic.Rdata"))
 load(here("processed-data/04_DEA/Tx_analysis/de_tx_smo.Rdata"))
@@ -55,13 +56,49 @@ DEtxs_genes_smo<-unique(de_tx_smo$ensembl_id)
 DEexons_genes_nic<-unique(de_exons_nic$gencodeID)
 DEexons_genes_smo<-unique(de_exons_smo$gencodeID)
 
-## Define groups of all DE jxn's genes
+## Define groups of all DE jxns' genes
 DEjxns_genes_nic <- unique(de_jxns_nic$newGeneID)[which(!is.na(unique(de_jxns_nic$newGeneID)))]
 DEjxns_genes_smo <- unique(de_jxns_smo$newGeneID)[which(!is.na(unique(de_jxns_smo$newGeneID)))]
 
+## Define groups of genes associated to Novel DE jxns
+novel_de_jxns_nic <- unique(de_jxns_nic[which(de_jxns_nic$Class=="Novel"), "newGeneID"])
+Novel_DEjxns_genes_nic <- novel_de_jxns_nic[which(!is.na(novel_de_jxns_nic))]
+novel_de_jxns_smo <- unique(de_jxns_smo[which(de_jxns_smo$Class=="Novel"), "newGeneID"])
+Novel_DEjxns_genes_smo <- novel_de_jxns_smo[which(!is.na(novel_de_jxns_smo))]
+
+## Define groups of the nearest genes of Novel DE jxns without associated gene
+nearest_genes_nic <- unique(novel_jxns_foundGenes[["nearest_genes_nic"]]$geneIdVersion)
+nearest_genes_smo <- unique(novel_jxns_foundGenes[["nearest_genes_smo"]]$geneIdVersion)
+
+## Define groups of the following genes of Novel DE jxns without associated gene
+following_genes_nic <- unique(novel_jxns_foundGenes[["following_genes_nic"]]$geneIdVersion)
+following_genes_smo <- unique(novel_jxns_foundGenes[["following_genes_smo"]]$geneIdVersion)
+
+## Define groups of the preceding genes of Novel DE jxns without associated gene
+preceding_genes_nic <- unique(novel_jxns_foundGenes[["preceding_genes_nic"]]$geneIdVersion)
+preceding_genes_smo <- unique(novel_jxns_foundGenes[["preceding_genes_smo"]]$geneIdVersion)
+
+## Define groups of AltStartEnd DE jxns' genes
+alt_de_jxns_nic <- unique(de_jxns_nic[which(de_jxns_nic$Class=="AltStartEnd"), "newGeneID"])
+alt_de_jxns_smo <- unique(de_jxns_smo[which(de_jxns_smo$Class=="AltStartEnd"), "newGeneID"])
+
+## Define groups of ExonSkip DE jxns' genes
+exonSkip_de_jxns_nic <- unique(de_jxns_nic[which(de_jxns_nic$Class=="ExonSkip"), "newGeneID"])
+exonSkip_de_jxns_smo <- unique(de_jxns_smo[which(de_jxns_smo$Class=="ExonSkip"), "newGeneID"])
+
+## Define groups of InGen DE jxns' genes
+inGen_de_jxns_nic <- unique(de_jxns_nic[which(de_jxns_nic$Class=="InGen"), "newGeneID"])
+inGen_de_jxns_smo <- unique(de_jxns_smo[which(de_jxns_smo$Class=="InGen"), "newGeneID"])
 
 
-## Compare DE txs' genes vs DE exons' genes vs DE jxns' genes vs DEG
+
+
+
+## Venn diagrams
+
+#################################################################################################
+## Compare DEG vs DE txs' genes vs DE exons' genes vs All DE jxns' genes (from all jxn classes)
+#################################################################################################
 
 ## All genes
 DEG_vs_Txs_vs_Exons_vs_Jxns_all <-list(
@@ -87,10 +124,281 @@ DEG_vs_Txs_vs_Exons_vs_Jxns_smo <-list(
   "DE jxns' genes"=DEjxns_genes_smo
 )
 
-
-## Venn diagrams
 DE_lists<-list(DEG_vs_Txs_vs_Exons_vs_Jxns_all, DEG_vs_Txs_vs_Exons_vs_Jxns_nic, DEG_vs_Txs_vs_Exons_vs_Jxns_smo)
 colors<-list(c("rosybrown2", "navajowhite2", "lightblue3", "darkolivegreen2"), c("plum", "lightgoldenrod2", "lightcyan3", "darkolivegreen3"), 
              c("pink1", "khaki2", "lightsteelblue3", "darkolivegreen4"))
-venn_plot(DE_lists, colors, "DEG_VS_txs_VS_exons_VS_jxns", c("All", "Nicotine", "Smoking"))
+venn_plot(DE_lists, colors, "DEG_VS_txs_VS_exons_VS_jxns_All", c("All", "Nicotine", "Smoking"))
+
+
+
+##########################################################################################
+## Compare DEG vs DE txs' genes vs DE exons' genes vs genes associated to Novel DE jxns
+##########################################################################################
+
+## All genes
+DEG_vs_Txs_vs_Exons_vs_NovelAssignedJxns_all <-list(
+  "DEG"= union(DEG_nic, DEG_smo),
+  "DE txs' genes"=union(DEtxs_genes_nic, DEtxs_genes_smo),
+  "DE exons' genes"=union(DEexons_genes_nic, DEexons_genes_smo),
+  "Genes assigned to Novel DE jxns"=union(Novel_DEjxns_genes_nic, Novel_DEjxns_genes_smo)                       
+)
+
+## Nic genes
+DEG_vs_Txs_vs_Exons_vs_NovelAssignedJxns_nic <-list(
+  "DEG"= DEG_nic,
+  "DE txs' genes"=DEtxs_genes_nic,
+  "DE exons' genes"=DEexons_genes_nic,
+  "Genes assigned to Novel DE jxns"=Novel_DEjxns_genes_nic
+)
+
+## Smo genes
+DEG_vs_Txs_vs_Exons_vs_NovelAssignedJxns_smo <-list(
+  "DEG"= DEG_smo,
+  "DE txs' genes"=DEtxs_genes_smo,
+  "DE exons' genes"=DEexons_genes_smo,
+  "Genes assigned to Novel DE jxns"=Novel_DEjxns_genes_smo
+)
+
+DE_lists<-list(DEG_vs_Txs_vs_Exons_vs_NovelAssignedJxns_all, DEG_vs_Txs_vs_Exons_vs_NovelAssignedJxns_nic, DEG_vs_Txs_vs_Exons_vs_NovelAssignedJxns_smo)
+colors<-list(c("rosybrown2", "navajowhite2", "lightblue3", "darkolivegreen2"), c("plum", "lightgoldenrod2", "lightcyan3", "darkolivegreen3"), 
+             c("pink1", "khaki2", "lightsteelblue3", "darkolivegreen4"))
+venn_plot(DE_lists, colors, "DEG_VS_txs_VS_exons_VS_jxns_NovelAssigned", c("All", "Nicotine", "Smoking"))
+
+
+
+######################################################################################
+## Compare DEG vs DE txs' genes vs DE exons' genes vs nearest genes to Novel DE jxns
+######################################################################################
+
+## All genes
+DEG_vs_Txs_vs_Exons_vs_NovelNearestJxns_all <-list(
+  "DEG"= union(DEG_nic, DEG_smo),
+  "DE txs' genes"=union(DEtxs_genes_nic, DEtxs_genes_smo),
+  "DE exons' genes"=union(DEexons_genes_nic, DEexons_genes_smo),
+  "Nearest genes to Novel DE jxns"=union(nearest_genes_nic, nearest_genes_smo)                       
+)
+
+## Nic genes
+DEG_vs_Txs_vs_Exons_vs_NovelNearestJxns_nic <-list(
+  "DEG"= DEG_nic,
+  "DE txs' genes"=DEtxs_genes_nic,
+  "DE exons' genes"=DEexons_genes_nic,
+  "Nearest genes to Novel DE jxns"= nearest_genes_nic
+)
+
+## Smo genes
+DEG_vs_Txs_vs_Exons_vs_NovelNearestJxns_smo <-list(
+  "DEG"= DEG_smo,
+  "DE txs' genes"=DEtxs_genes_smo,
+  "DE exons' genes"=DEexons_genes_smo,
+  "Nearest genes to Novel DE jxns"= nearest_genes_smo
+)
+
+DE_lists<-list(DEG_vs_Txs_vs_Exons_vs_NovelNearestJxns_all, DEG_vs_Txs_vs_Exons_vs_NovelNearestJxns_nic, DEG_vs_Txs_vs_Exons_vs_NovelNearestJxns_smo)
+colors<-list(c("rosybrown2", "navajowhite2", "lightblue3", "darkolivegreen2"), c("plum", "lightgoldenrod2", "lightcyan3", "darkolivegreen3"), 
+             c("pink1", "khaki2", "lightsteelblue3", "darkolivegreen4"))
+venn_plot(DE_lists, colors, "DEG_VS_txs_VS_exons_VS_jxns_NovelNearest", c("All", "Nicotine", "Smoking"))
+
+
+
+########################################################################################
+## Compare DEG vs DE txs' genes vs DE exons' genes vs following genes to Novel DE jxns
+########################################################################################
+
+## All genes
+DEG_vs_Txs_vs_Exons_vs_NovelFollowingJxns_all <-list(
+  "DEG"= union(DEG_nic, DEG_smo),
+  "DE txs' genes"=union(DEtxs_genes_nic, DEtxs_genes_smo),
+  "DE exons' genes"=union(DEexons_genes_nic, DEexons_genes_smo),
+  "Following genes to Novel DE jxns"=union(following_genes_nic, following_genes_smo)                       
+)
+
+## Nic genes
+DEG_vs_Txs_vs_Exons_vs_NovelFollowingJxns_nic <-list(
+  "DEG"= DEG_nic,
+  "DE txs' genes"=DEtxs_genes_nic,
+  "DE exons' genes"=DEexons_genes_nic,
+  "Following genes to Novel DE jxns"= following_genes_nic
+)
+
+## Smo genes
+DEG_vs_Txs_vs_Exons_vs_NovelFollowingJxns_smo <-list(
+  "DEG"= DEG_smo,
+  "DE txs' genes"=DEtxs_genes_smo,
+  "DE exons' genes"=DEexons_genes_smo,
+  "Following genes to Novel DE jxns"= following_genes_smo
+)
+
+DE_lists<-list(DEG_vs_Txs_vs_Exons_vs_NovelFollowingJxns_all, DEG_vs_Txs_vs_Exons_vs_NovelFollowingJxns_nic, DEG_vs_Txs_vs_Exons_vs_NovelFollowingJxns_smo)
+colors<-list(c("rosybrown2", "navajowhite2", "lightblue3", "darkolivegreen2"), c("plum", "lightgoldenrod2", "lightcyan3", "darkolivegreen3"), 
+             c("pink1", "khaki2", "lightsteelblue3", "darkolivegreen4"))
+venn_plot(DE_lists, colors, "DEG_VS_txs_VS_exons_VS_jxns_NovelFollowing", c("All", "Nicotine", "Smoking"))
+
+
+
+########################################################################################
+## Compare DEG vs DE txs' genes vs DE exons' genes vs preceding genes to Novel DE jxns
+########################################################################################
+
+## All genes
+DEG_vs_Txs_vs_Exons_vs_NovelPrecedingJxns_all <-list(
+  "DEG"= union(DEG_nic, DEG_smo),
+  "DE txs' genes"=union(DEtxs_genes_nic, DEtxs_genes_smo),
+  "DE exons' genes"=union(DEexons_genes_nic, DEexons_genes_smo),
+  "Preceding genes to Novel DE jxns"=union(preceding_genes_nic, preceding_genes_smo)                       
+)
+
+## Nic genes
+DEG_vs_Txs_vs_Exons_vs_NovelPrecedingJxns_nic <-list(
+  "DEG"= DEG_nic,
+  "DE txs' genes"=DEtxs_genes_nic,
+  "DE exons' genes"=DEexons_genes_nic,
+  "Preceding genes to Novel DE jxns"= preceding_genes_nic
+)
+
+## Smo genes
+DEG_vs_Txs_vs_Exons_vs_NovelPrecedingJxns_smo <-list(
+  "DEG"= DEG_smo,
+  "DE txs' genes"=DEtxs_genes_smo,
+  "DE exons' genes"=DEexons_genes_smo,
+  "Preceding genes to Novel DE jxns"= preceding_genes_smo
+)
+
+DE_lists<-list(DEG_vs_Txs_vs_Exons_vs_NovelPrecedingJxns_all, DEG_vs_Txs_vs_Exons_vs_NovelPrecedingJxns_nic, DEG_vs_Txs_vs_Exons_vs_NovelPrecedingJxns_smo)
+colors<-list(c("rosybrown2", "navajowhite2", "lightblue3", "darkolivegreen2"), c("plum", "lightgoldenrod2", "lightcyan3", "darkolivegreen3"), 
+             c("pink1", "khaki2", "lightsteelblue3", "darkolivegreen4"))
+venn_plot(DE_lists, colors, "DEG_VS_txs_VS_exons_VS_jxns_NovelPreceding", c("All", "Nicotine", "Smoking"))
+
+
+
+###################################################################################
+## Compare DEG vs DE txs' genes vs DE exons' genes vs AltStartEnd DE jxns' genes 
+###################################################################################
+
+## All genes
+DEG_vs_Txs_vs_Exons_vs_AltJxns_all <-list(
+  "DEG"= union(DEG_nic, DEG_smo),
+  "DE txs' genes"=union(DEtxs_genes_nic, DEtxs_genes_smo),
+  "DE exons' genes"=union(DEexons_genes_nic, DEexons_genes_smo),
+  "AltStartEnd DE jxns' genes"=union(alt_de_jxns_nic, alt_de_jxns_smo)                       
+)
+
+## Nic genes
+DEG_vs_Txs_vs_Exons_vs_AltJxns_nic <-list(
+  "DEG"= DEG_nic,
+  "DE txs' genes"=DEtxs_genes_nic,
+  "DE exons' genes"=DEexons_genes_nic,
+  "AltStartEnd DE jxns' genes"=alt_de_jxns_nic
+)
+
+## Smo genes
+DEG_vs_Txs_vs_Exons_vs_AltJxns_smo <-list(
+  "DEG"= DEG_smo,
+  "DE txs' genes"=DEtxs_genes_smo,
+  "DE exons' genes"=DEexons_genes_smo,
+  "AltStartEnd DE jxns' genes"=alt_de_jxns_smo
+)
+
+DE_lists<-list(DEG_vs_Txs_vs_Exons_vs_AltJxns_all, DEG_vs_Txs_vs_Exons_vs_AltJxns_nic, DEG_vs_Txs_vs_Exons_vs_AltJxns_smo)
+colors<-list(c("rosybrown2", "navajowhite2", "lightblue3", "darkolivegreen2"), c("plum", "lightgoldenrod2", "lightcyan3", "darkolivegreen3"), 
+             c("pink1", "khaki2", "lightsteelblue3", "darkolivegreen4"))
+venn_plot(DE_lists, colors, "DEG_VS_txs_VS_exons_VS_jxns_AltStartEnd", c("All", "Nicotine", "Smoking"))
+
+
+
+###################################################################################
+## Compare DEG vs DE txs' genes vs DE exons' genes vs InGen DE jxns' genes 
+###################################################################################
+
+## All genes
+DEG_vs_Txs_vs_Exons_vs_InGenJxns_all <-list(
+  "DEG"= union(DEG_nic, DEG_smo),
+  "DE txs' genes"=union(DEtxs_genes_nic, DEtxs_genes_smo),
+  "DE exons' genes"=union(DEexons_genes_nic, DEexons_genes_smo),
+  "InGen DE jxns' genes"=union(inGen_de_jxns_nic, inGen_de_jxns_smo)                       
+)
+
+## Nic genes
+DEG_vs_Txs_vs_Exons_vs_InGenJxns_nic <-list(
+  "DEG"= DEG_nic,
+  "DE txs' genes"=DEtxs_genes_nic,
+  "DE exons' genes"=DEexons_genes_nic,
+  "InGen DE jxns' genes"=inGen_de_jxns_nic
+)
+
+## Smo genes
+DEG_vs_Txs_vs_Exons_vs_InGenJxns_smo <-list(
+  "DEG"= DEG_smo,
+  "DE txs' genes"=DEtxs_genes_smo,
+  "DE exons' genes"=DEexons_genes_smo,
+  "InGen DE jxns' genes"=inGen_de_jxns_smo
+)
+
+DE_lists<-list(DEG_vs_Txs_vs_Exons_vs_InGenJxns_all, DEG_vs_Txs_vs_Exons_vs_InGenJxns_nic, DEG_vs_Txs_vs_Exons_vs_InGenJxns_smo)
+colors<-list(c("rosybrown2", "navajowhite2", "lightblue3", "darkolivegreen2"), c("plum", "lightgoldenrod2", "lightcyan3", "darkolivegreen3"), 
+             c("pink1", "khaki2", "lightsteelblue3", "darkolivegreen4"))
+venn_plot(DE_lists, colors, "DEG_VS_txs_VS_exons_VS_jxns_InGen", c("All", "Nicotine", "Smoking"))
+
+
+
+###################################################################################
+## Compare DEG vs DE txs' genes vs DE exons' genes vs ExonSkip DE jxns' genes 
+###################################################################################
+
+## All genes
+DEG_vs_Txs_vs_Exons_vs_ExonSkipJxns_all <-list(
+  "DEG"= union(DEG_nic, DEG_smo),
+  "DE txs' genes"=union(DEtxs_genes_nic, DEtxs_genes_smo),
+  "DE exons' genes"=union(DEexons_genes_nic, DEexons_genes_smo),
+  "ExonSkip DE jxns' genes"=union(exonSkip_de_jxns_nic, exonSkip_de_jxns_smo)                       
+)
+
+## Nic genes
+DEG_vs_Txs_vs_Exons_vs_ExonSkipJxns_nic <-list(
+  "DEG"= DEG_nic,
+  "DE txs' genes"=DEtxs_genes_nic,
+  "DE exons' genes"=DEexons_genes_nic,
+  "ExonSkip DE jxns' genes"=exonSkip_de_jxns_nic
+)
+
+## Smo genes
+DEG_vs_Txs_vs_Exons_vs_ExonSkipJxns_smo <-list(
+  "DEG"= DEG_smo,
+  "DE txs' genes"=DEtxs_genes_smo,
+  "DE exons' genes"=DEexons_genes_smo,
+  "ExonSkip DE jxns' genes"=exonSkip_de_jxns_smo
+)
+
+DE_lists<-list(DEG_vs_Txs_vs_Exons_vs_ExonSkipJxns_all, DEG_vs_Txs_vs_Exons_vs_ExonSkipJxns_nic, DEG_vs_Txs_vs_Exons_vs_ExonSkipJxns_smo)
+colors<-list(c("rosybrown2", "navajowhite2", "lightblue3", "darkolivegreen2"), c("plum", "lightgoldenrod2", "lightcyan3", "darkolivegreen3"), 
+             c("pink1", "khaki2", "lightsteelblue3", "darkolivegreen4"))
+venn_plot(DE_lists, colors, "DEG_VS_txs_VS_exons_VS_jxns_ExonSkip", c("All", "Nicotine", "Smoking"))
+
+
+
+
+
+
+
+## Reproducibility information
+print('Reproducibility information:')
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
+
+# setting  value
+# version  R version 4.2.2 (2022-10-31)
+# os       macOS Monterey 12.5.1
+# system   aarch64, darwin20
+# ui       RStudio
+# language (EN)
+# collate  en_US.UTF-8
+# ctype    en_US.UTF-8
+# tz       America/Mexico_City
+# date     2023-03-02
+# rstudio  2022.12.0+353 Elsbeth Geranium (desktop)
+# pandoc   NA
+
+
 
