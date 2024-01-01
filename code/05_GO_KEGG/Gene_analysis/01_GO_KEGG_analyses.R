67a49d5bdac825079c180e069cd1ad217400eed6
daianna21 <daianna2121@outlook.es>
2024-01-01 16:03:09 -0600

    New boxplots of the top 6 genes in each group without cleaningY()

--- code/05_GO_KEGG/Gene_analysis/01_GO_KEGG_analyses.R
+++ code/05_GO_KEGG/Gene_analysis/01_GO_KEGG_analyses.R
@@ -250,14 +250,7 @@ save(goList_intersections, file="processed-data/05_GO_KEGG/Gene_analysis/goList_
 ## Lognorm counts of genes in both nicotine and smoking fitted models
 vGene_smo<-results_pups_smoking_fitted[[1]][[2]]
 vGene_nic<-results_pups_nicotine_fitted[[1]][[2]]
-## Regress out residuals 
-formula<- ~ Group + Sex + plate + flowcell + rRNA_rate + totalAssignedGene + ERCCsumLogErr + 
-  overallMapRate + mitoRate
-model<- model.matrix(formula, data=colData(rse_gene_brain_pups_smoking))
-vGene_smo$E<-cleaningY(vGene_smo$E, model, P=2)
 rownames(vGene_nic$E)<-vGene_nic$genes$Symbol
-model<- model.matrix(formula, data=colData(rse_gene_brain_pups_nicotine))
-vGene_nic$E<-cleaningY(vGene_nic$E, model, P=2)
 rownames(vGene_smo$E)<-vGene_smo$genes$Symbol
 
 
@@ -365,24 +358,28 @@ DEG_GO_boxplot <- function(DEgene){
 
   ## Boxplot for each DE gene
   p <-ggplot(data=as.data.frame(df), aes(x=Group,y=Gene_counts)) + 
-      geom_boxplot(outlier.color = "#FFFFFFFF") +
-      geom_jitter(aes(color=Group), position=position_jitter(0.2)) +
-      theme_classic() +
-      labs(x = "Experiment", y = "logcounts - covariates",
+      geom_boxplot(outlier.color = "#FFFFFFFF", width=0.35) +
+      geom_jitter(aes(color=Group), shape=16, position=position_jitter(0.2), size=2.1) +
+      theme_bw() +
+      labs(x = "Experiment", y = "lognorm counts",
            title = paste(DEgene, ensemblID, sep=" - "),
            subtitle=" ") +
-      theme(plot.margin=unit (c (1,1.5,1,1), 'cm'), legend.position = "none",
-            plot.title = element_text(hjust=0.5, size=10, face="bold"), 
-            plot.subtitle = element_text(size=17)) +
-      scale_color_manual(values = c("orangered", "Dodgerblue")) +
+     scale_color_manual(values=c("Control" = "seashell3", "Experimental" = "orange3")) +
+     scale_x_discrete(labels=c("Control"="Ctrl","Experimental"="Expt")) +
       facet_wrap(~ Expt, scales = "free") +
-      scale_x_discrete(labels=c("Ctrl", "Expt"))
-  
+      scale_x_discrete(labels=c("Ctrl", "Expt")) +
+    theme(plot.margin=unit (c (1,1.5,1,1), 'cm'), 
+          legend.position = "none",
+          plot.title = element_text(hjust=0.5, size=12, face="bold"), 
+          plot.subtitle = element_text(size=17), 
+          axis.title = element_text(size = (12)),
+          axis.text = element_text(size = 10.5)) 
+
   p <-ggdraw(p) + 
-      draw_label(paste("FDR:", q_value_nic), x = 0.35, y = 0.87, size=9, color = "darkslategray") +
-      draw_label(paste("FC:", FC_nic), x = 0.35, y = 0.84, size=9, color = "darkslategray") +
-      draw_label(paste("FDR:", q_value_smo), x = 0.72, y = 0.87, size=9, color = "darkslategray") +
-      draw_label(paste("FC:", FC_smo), x = 0.71, y = 0.84, size=9, color = "darkslategray") 
+      draw_label(paste("FDR:", q_value_nic), x = 0.35, y = 0.83, size=9, color = "darkslategray") +
+      draw_label(paste("FC:", FC_nic), x = 0.35, y = 0.80, size=9, color = "darkslategray") +
+      draw_label(paste("FDR:", q_value_smo), x = 0.72, y = 0.83, size=9, color = "darkslategray") +
+      draw_label(paste("FC:", FC_smo), x = 0.71, y = 0.80, size=9, color = "darkslategray") 
   
   return(p)
   
@@ -401,7 +398,7 @@ GO_boxplots<- function (DEG_list, groups){
   }
   plot_grid(plots[[1]], plots[[2]], plots[[3]], plots[[4]], plots[[5]], plots[[6]], ncol=3)
   ggsave(here(paste("plots/05_GO_KEGG/Gene_analysis/Top", length(DEG_list), "_DEG_boxplots_", groups, ".pdf", sep="")), 
-         width = 40, height = 25, units = "cm") 
+         width = 40, height = 20, units = "cm") 
 }
 
 
